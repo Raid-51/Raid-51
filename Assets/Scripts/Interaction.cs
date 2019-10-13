e23517a0444cf316725093a36de4b75337464453
MikaelAndriIngason <33830730+MikaelAndriIngason@users.noreply.github.com>
2019-10-13 17:29:53 +0000

    Comments

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
+    //range til að sjá hluti
     public float Range;
 
     private Camera cam;
@@ -13,6 +14,7 @@ public class Interaction : MonoBehaviour
 
     void Start()
     {
+        //Nær í componentana
         cam = GetComponentInChildren<Camera>();
         IF = GameObject.FindGameObjectWithTag("Interface").GetComponent<Interface>();
         INV = IF.gameObject.GetComponentInChildren<Inventory>();
@@ -23,52 +25,60 @@ public class Interaction : MonoBehaviour
 
     void Update()
     {
+        //Skýtur raycast  í miðju skjás 
         RaycastHit hit;
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
+        //Ef það er hlutur í raycastinu
         if (Physics.Raycast(ray, out hit, Range))
         {
+            //Ef það hittir hlut sem er hægt að taka upp
             if (hit.collider.tag == "Object")
             {
-                IF.Interacting(0);
-                if (Input.GetMouseButtonDown(0))
+                IF.Interacting(0); //Sýnir texta
+                if (Input.GetMouseButtonDown(0)) //Ef spilarinn "interactar" við hlutinn (smellir á mús)
                 {
-                    if (!INV.Fullinventory)
+                    if (!INV.Fullinventory) //Ef inventory-ið er ekki fullt
                     {
-                        INV.AddToInventory(hit.collider.GetComponent<Object>().ObjectID);
-                        Destroy(hit.collider.gameObject);
+                        INV.AddToInventory(hit.collider.GetComponent<Object>().ObjectID); //Setur hlutinn í inventory-ið
+                        Destroy(hit.collider.gameObject); //Eyðir hlutinum úr veröldinni
                     }
                 }
 
             }
+            //Ef það hittir girðingu sem er hægt að brjóta
             else if (hit.collider.tag == "Breakable fence")
             {
-                if (INV.CurrentItemID() == 2)
+                if (INV.CurrentItemID() == 2) //Ef hluturinn er vírklippur í höndinni á spilaranum
                 {
-                    IF.Interacting(4);
-                    if (Input.GetMouseButtonDown(0))
+                    IF.Interacting(4); //Sýnir texta
+                    if (Input.GetMouseButtonDown(0)) //Ef spilarinn "interactar" við hlutinn (smellir á mús)
                     {
-                        Breakable_fence fence_hit_script = hit.collider.gameObject.GetComponent<Breakable_fence>();
+                        Breakable_fence fence_hit_script = hit.collider.gameObject.GetComponent<Breakable_fence>(); //Nær í fence scriptina
 
-                        fence_hit_script.destroyed = true;
+                        fence_hit_script.destroyed = true; //Opnar girðinguna
                     }
                 }
             }
+            //Ef það hittir hurð sem er hægt að opna
             else if (hit.collider.tag == "Door")
             {
 
             }
+            //Ef það hittir liftu sem er hægt að fara í
             else if (hit.collider.tag == "Elevator")
             {
 
             }
+            //Ef það hittir lás sem er hægt að opna
             else if (hit.collider.tag == "Lock")
             {
 
             }
+            //Ef það hittir ekki neitt þá er spilarinn ekki að interacta við neitt
             else
                 IF.NotInteracting();
         }
+        //Ef það hittir ekki neitt þá er spilarinn ekki að interacta við neitt
         else
             IF.NotInteracting();
     }
