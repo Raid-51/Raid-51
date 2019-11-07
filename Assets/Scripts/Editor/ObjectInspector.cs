f87211dcf1197fcb96a7b55cf339d4705721390c
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-11-07 15:28:33 +0000

    Takki kominn til þess að geta objects að objects, þarf bara að finna hvernig maður keyrir þetta automatically

---
+++ Assets/Scripts/Editor/ObjectInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Object))]
+public class ObjectInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Set As Object"))
+        {
+            Object pickupScript = (Object)target;
+
+            pickupScript.gameObject.tag = "Object";
+            pickupScript.SceneNumber = pickupScript.gameObject.scene.buildIndex;
+        }
+    }
+}
