4d7a08686131dbee647b877e8013837b89d17bce
Mikael Andri Ingason <mikaelin258@nemi.tskoli.is>
2019-11-14 14:07:32 +0000

    objectie marker

---
+++ Assets/Objective.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.LookAt(2* transform.position - new Vector3(player.position.x, player.position.y + 1, player.position.z));
+    }
+}
