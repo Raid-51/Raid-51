31d9242de2049f0ca315a4bd8f95d2b0e73f08e8
Mikael Andri Ingason <mikaelin258@nemi.tskoli.is>
2019-11-14 15:07:35 +0000

    Objectives

--- Assets/Objective.cs
+++ Assets/Objective.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Objective : MonoBehaviour
 {
+    public float UIScale = 0.275f;
     private Transform player;
+    private Vector3 initialScale;
+    private Camera Cam;
 
     void Start()
     {
+        initialScale = transform.localScale;
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        Cam = Camera.main;
     }
 
     void Update()
     {
+        Plane plane = new Plane(Cam.transform.forward, Cam.transform.position);
+        float dist = plane.GetDistanceToPoint(transform.position);
+        transform.localScale = initialScale * dist * UIScale;
+
         transform.LookAt(2* transform.position - new Vector3(player.position.x, player.position.y + 1, player.position.z));
     }
 }
---
+++ Assets/Objectives.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives : MonoBehaviour
+{
+    public int CurrentObjective;
+
+    [Space]
+    public GameObject[] AllObjectives;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AllObjectives[CurrentObjective].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ObjectiveFinished()
+    {
+        AllObjectives[CurrentObjective].SetActive(false);
+        CurrentObjective += 1;
+        AllObjectives[CurrentObjective].SetActive(true);
+    }
+}
