691008e402074c0dc873a75986a97149dd350a6f
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-12-01 21:10:10 +0000

    Bunkerar lokast þegar spilarinn fer úr þeim og það er sýnt með plankum

--- Assets/Scripts/SwitchSceneDoor.cs
+++ Assets/Scripts/SwitchSceneDoor.cs
@@ -57,7 +57,8 @@ public class SwitchSceneDoor : MonoBehaviour
     public void CloseDoor()
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
-        this.gameObject.GetComponentInChildren<Opendoor>().gameObject.GetComponent<BoxCollider>();
+        this.gameObject.GetComponentInChildren<Opendoor>().gameObject.SetActive(false);
+        this.gameObject.transform.parent.GetChild(1).gameObject.SetActive(true);
         this.enabled = false;
     }
 }
